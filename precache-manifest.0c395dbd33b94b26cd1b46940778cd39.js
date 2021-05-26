670bcfb32a4b25f46dc1351cad8e7acdb3b26fc3
Inna Golovina <igigig978@gmail.com>
2021-05-26 17:54:35 +0300

    Updates

---
+++ precache-manifest.0c395dbd33b94b26cd1b46940778cd39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5965288dc8783e34644d",
+    "url": "/movies_app_redux/static/css/main.b46f1209.chunk.css"
+  },
+  {
+    "revision": "5965288dc8783e34644d",
+    "url": "/movies_app_redux/static/js/main.5965288d.chunk.js"
+  },
+  {
+    "revision": "0f753e04173c03b83f85",
+    "url": "/movies_app_redux/static/js/1.0f753e04.chunk.js"
+  },
+  {
+    "revision": "c7952e36604c1c08cd3d",
+    "url": "/movies_app_redux/static/js/runtime~main.c7952e36.js"
+  },
+  {
+    "revision": "00ff99c891bc791f526096503099cf66",
+    "url": "/movies_app_redux/index.html"
+  }
+];
